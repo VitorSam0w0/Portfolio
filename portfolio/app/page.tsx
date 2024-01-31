06975d76576b3ecf4fbbad89dc3d336d42dd4b2f
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2024-01-31 08:24:06 -0300

    Update

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,17 +4,17 @@ import Logo from "./img/logo.png"
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-between p-24">
+    <main className="flex items-center justify-between p-24 text-gray-500 text-2xl">
       <div className="flex items-center">
         <div>
           <Image className="w-20 h-20" src={Logo} alt="Imagem logo"></Image>
         </div>
-        <div className="text-end">
-          <ul className="flex justify-right">
-            <li>
+        <div>
+          <ul className="flex justify-end">
+            <li className="hover:text-black">
               <Link className="px-11" href="/Projetos">+Projetos</Link>
             </li>
-            <li className="transform hover:scale-110 transition-transform easy-in-out ">
+            <li className="transform hover:scale-110 ease-in duration-300 hover:ease-in">
               <Link className="px-11 rounded-full border-gray-950 border-2 hover:bg-black hover:text-white " href="/Email">Email</Link>
             </li>
           </ul>
