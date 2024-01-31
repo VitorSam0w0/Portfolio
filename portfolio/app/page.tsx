20929515d95c666e0604c70b493be9c060fb098d
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2024-01-31 06:36:06 -0300

    Novas paginas

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="container mx-auto px-18">
-        <p>teste</p>
+        <ul>
+          <li><Link href="/Projetos">+Projetos</Link></li>git 
+          <li><Link href="/Email">E-mail</Link></li>
+        </ul>
       </div>
     </main>
   );
