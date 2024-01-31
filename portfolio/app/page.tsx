d81aed193193bff58c329670c222269768ed03b1
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2024-01-31 06:42:43 -0300

    Up

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "./img/logo.png"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="container mx-auto px-18">
         <ul>
+          <li><Image className="" src={Logo} alt="Imagem logo"></Image></li>
           <li><Link href="/Projetos">+Projetos</Link></li>git 
           <li><Link href="/Email">E-mail</Link></li>
         </ul>
