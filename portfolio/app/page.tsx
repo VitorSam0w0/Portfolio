f4c24c21d6be7a646625411b2b8f5359b69c0148
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2024-02-05 00:10:20 -0300

    update

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,4 +1,3 @@
-// index.js
 import NavBar from "@/pages/NavBar";
 
 const Home = () => {
@@ -6,9 +5,9 @@ const Home = () => {
     <div className="flex flex-col min-h-screen font-mono bg-gray-100">
       <NavBar />
       <main className="m-20 p-20">
-        <div className="w-50 h-30 bg-gray-600"></div>
-        <div className="w-50 h-30 bg-gray-600"></div>
-        <div className="w-50 h-30 bg-gray-600"></div>
+        <h1>
+        </h1>
+        
       </main>
       <footer className="bg-gray-800 text-white p-4 text-center">
         © 2024 Portfolio de Vitor Dias
--- portfolio/pages/Projetos.tsx
+++ portfolio/pages/Projetos.tsx
@@ -6,12 +6,8 @@ export default function Projetos() {
     <div className="min-h-screen flex flex-col bg-gray-100 font-mono ">
       <NavBar/>
       <main className="m-20 p-20">
-        <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Meu Site</h1>
-        <p className="text-lg">
-        </p>
       </main>
 
-      {/* Rodapé */}
       <footer className="bg-gray-800 text-white p-4 text-center">
         © 2024 Portfolio de Vitor Dias
       </footer>
