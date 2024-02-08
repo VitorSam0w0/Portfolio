2112f5aba9a2265fc3d7f3148d704316a7e0f68e
Vitor Dias <111717270+VitorSam0w0@users.noreply.github.com>
2024-02-07 22:33:38 -0300

    push

--- portfolio/pages/Blocos/BlocosProjetoCalorias.tsx
+++ portfolio/pages/Blocos/BlocosProjetoCalorias.tsx
@@ -5,7 +5,7 @@ import CapaFrutas from "../../img/capaFrutas.png"
 const BlocosProjetoCalorias = () => {
     return(
         <div>
-          <div className="m-4 p-3 rounded-md bg-gray-300">Em Breve<div><Link href="/SiteCalorias"><Image className="ease-in-out duration-300 hover:scale-90 w-full rounded-lg w-48 h-20 md:min-w-96 md:min-h-40" src={CapaFrutas} alt="Site de Calorias"></Image></Link></div><strong>Projeto de site Calculadora de calorias</strong></div>
+          <div className="m-4 p-3 rounded-md bg-gray-300">Em Breve<div><Link href="/SiteCalorias"><Image className="ease-in-out duration-300 hover:scale-90 w-full rounded-lg w-48 h-28 lg:min-w-96 md:min-h-40" src={CapaFrutas} alt="Site de Calorias"></Image></Link></div><strong>Projeto de site Calculadora de calorias</strong></div>
         </div>   
     )
 }
--- portfolio/pages/Blocos/BlocosProjetoReclameAqui.tsx
+++ portfolio/pages/Blocos/BlocosProjetoReclameAqui.tsx
@@ -5,7 +5,7 @@ import capaRA from "../../img/capaRA.png"
 const BlocosProjetoReclameAqui = () => {
     return(
         <div>
-            <div className="m-4 p-3 rounded-md bg-gray-300">Em Breve<div><Link href="/ReclameAqui"><Image className="ease-in-out duration-300 hover:scale-90 w-full rounded-lg w-48 h-20 md:min-w-96 md:min-h-40" src={capaRA} alt="Site de Reclame Aqui"></Image></Link></div><p><strong>Projeto de site Reclame Aqui</strong></p></div>
+            <div className="m-4 p-3 rounded-md bg-gray-300">Em Breve<div><Link href="/ReclameAqui"><Image className="ease-in-out duration-300 hover:scale-90 w-full rounded-lg w-48 h-28 lg:min-w-96 md:min-h-40" src={capaRA} alt="Site de Reclame Aqui"></Image></Link></div><p><strong>Projeto de site Reclame Aqui</strong></p></div>
         </div>
     )
 }
